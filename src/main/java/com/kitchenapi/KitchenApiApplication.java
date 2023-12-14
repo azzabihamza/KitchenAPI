b01b3b0bd12be3103a17ead590ae11322de9a559
azzabihamza <hamza.azzabi@esprit.tn>
2023-12-14 11:43:12 +0100

    First commit project initialisation

---
+++ src/main/java/com/kitchenapi/KitchenApiApplication.java
@@ -0,0 +1,13 @@
+package com.kitchenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kitchenapi/KitchenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitchenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
